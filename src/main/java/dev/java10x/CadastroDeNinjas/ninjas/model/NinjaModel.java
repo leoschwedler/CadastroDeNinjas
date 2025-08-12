d6ad6c30208d074e8235124ec3d289a8507f8f01
leonardo schwuedller <77402907+leoschwedler@users.noreply.github.com>
2025-08-12 09:14:31 -0300

    refactor: include @ToString to models

--- src/main/java/dev/java10x/CadastroDeNinjas/ninjas/model/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/ninjas/model/NinjaModel.java
@@ -6,12 +6,14 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Table(name = "tb_cadastro")
 @NoArgsConstructor
 @Data
 @AllArgsConstructor
+@ToString
 public class NinjaModel {
 
     @Id
